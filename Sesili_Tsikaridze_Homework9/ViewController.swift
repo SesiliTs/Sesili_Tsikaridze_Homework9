786e7bc09d7db50817a649427da5f8d21b2cd0e3
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2023-01-22 11:47:13 +0400

    Initial Commit

---
+++ Sesili_Tsikaridze_Homework9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sesili_Tsikaridze_Homework9
+//
+//  Created by Sesili on 22.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
